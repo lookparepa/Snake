2308d3c4d1a5403e7147b0aa6c5e5bdb98fcfdcd
lookparepa <the.pa.re@hotmail.com>
2016-12-22 23:50:25 +0700

    add snake

--- Snake.py
+++ Snake.py
@@ -18,3 +18,21 @@ def getRandomPosition():
     return (randint(-SIZE//2, SIZE//2), randint(-SIZE//2, SIZE//2))
 
 food_coor = getRandomPosition()
+
+snake = turtle.Turtle()
+snake.up()
+snake.shape("square")
+snake.color("red")
+snake.ht()
+
+snake_coor = [(0, 0)]
+
+stamps = []
+
+dir_x = 0
+dir_y = 0
+
+stop = False
+
+
+
